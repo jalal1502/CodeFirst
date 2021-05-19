d9ce2ebdf9f2e92086275dcff3df3d65a888f877
dounia jalal <dounia_jalal@emsi-edu.ma>
2021-05-19 22:26:08 +0100

    First Push

---
+++ WebAppliCodeFirst/WebAppliCodeFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppliCodeFirst.Global" Language="C#" %>
---
+++ WebAppliCodeFirst/WebAppliCodeFirst/Models/Voiture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppliCodeFirst.Models
+{
+    public class Voiture
+    {
+        public Int32 id { get; set; }
+        public string Marque { get; set; }
+        public string Modele { get; set; }
+        public string NChassis { get; set; }
+    }
+}
